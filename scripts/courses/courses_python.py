c1fe1a63e5f0acfdc8e9313c1c7e95802606e2e2
stefraynaud <stephane.raynaud@gmail.com>
2013-09-04 11:52:16 +0000

    cours++

--- scripts/courses/courses_python.py
+++ scripts/courses/courses_python.py
@@ -20,21 +20,21 @@ print isinstance('a', str)
 # Indentations
 a = True
 if a is True:
-    print 'True !!!'        # -> ESSAYER SANS INDENTATION
+    print 'True !!!'        # -> ESSAYEZ SANS INDENTATION
 
 
 # Quelques types
 
 # - listes
 a = [1, 3] 
-a.append(5)                 # -> ESSAYER EXTEND()
+a.append(5)                 # -> ESSAYEZ EXTEND()
 a[0] = 10
 print a
 del a[1]
 
 # - tuples
 b = (5, 6)
-print b[1]                  # -> ESSAYER DE CHANGER B[0], DEL
+print b[1]                  # -> ESSAYEZ DE CHANGER B[0], DEL
 c = list(a)
 print b, c
 
@@ -45,6 +45,22 @@ c.update(d, e=5)
 print c.keys()
 print c
 
+# - chaines de caracteres
+a = 'a'
+b = "b"
+c = """
+e
+bc
+"""
+print "a=%s b=%i"%(3, 4)
+a=3; b=4
+print "a=%(a)s b=%(b)i"%dict(a=a, b=b)  # -> ESSAYEZ AVEC VARS()
+print 'jean dupont'.title()
+print '123'.rjust(100)                  # -> ESSAYEZ .CENTER()
+print 'a={} b={}'.format(a, b)
+print 'a={0} b={1:f}'.format(a, b)
+a = {'arg1':'ok', 'arg2':6}
+print 'arg1 de a = {a[arg1]}, arg2 = {a[arg2]}'.format(a=a) # on ne passe que a
 
 # Slicing
 a = range(10)
@@ -60,7 +76,8 @@ print a[slice(None, None, 2)]
 a = range(6)
 for i in a:
     print i
-for key, val in c.items():
+d = {'a':1,  'b':2}
+for key, val in d.items():
     print key, val
 # - while
 i = 0
@@ -86,9 +103,8 @@ b[0] = 100
 print a
 
 # - copy
-ac = a.copy()
 from copy import copy, deepcopy
-ac2 = copy(a)
+ac = copy(a)
 adc = deepcopy(a)
 
 
