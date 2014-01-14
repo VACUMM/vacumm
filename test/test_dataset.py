bdf2b3c4cdd5754429a1b03dd68b785debe1d5a9
stefraynaud <stephane.raynaud@gmail.com>
2014-01-14 10:27:04 +0000

    test: dataset

--- test/test_dataset.py
+++ test/test_dataset.py
@@ -5,15 +5,17 @@ class TestSequenceFunctions(VCTestCase):
 
     for test_name in [
         'test_dataset_get_temp_mfs',
-        'test_dataset_get_temp_enor',
-        'test_dataset_asvar_mfs',
-        'test_dataset_asvar_enor',
+        'test_dataset_get_temp_menor',
+        'test_dataset_get_asvar_mfs',
+        'test_dataset_get_asvar_menor',
+        'test_dataset_get_at',
         'test_dataset_get_dens',
+        'test_dataset_get_mld',
+        'test_dataset_get_uvbt_menor',
         'test_dataset_plot_transect_menor',
         'test_dataset_plot_transect_mfs',
         'test_dataset_plot_hsection_menor',
         'test_dataset_plot_hsection_mfs',
-        'test_dataset_get_uvgbt_menor',
         ]:
         exec(method_template.format(test_name))
         
