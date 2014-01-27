ec00e9c74d37c695c59433b157aa34b5665314aa
stefraynaud <stephane.raynaud@gmail.com>
2014-01-27 10:03:53 +0000

    tests: missing scripts

---
+++ scripts/test/test_regrid_transect_xyt.py
@@ -0,0 +1,42 @@
+"""Test :func:`~vacumm.misc.grid.regridding.transect` with interpolation in both time and space"""
+
+# Inits
+ncfile = "mars2d.xyt.nc"
+lon0 = -5.4
+lon1 = -4.7
+lat0 = 48.1
+lat1 = 48.6
+time0 = "2008-08-15 07:00"
+time1 = "2008-08-15 16:00"
+
+
+# Imports
+from vcmq import cdms2, data_sample, N, transect, stick2, code_base_name, os, \
+    transect_specs, add_map_lines, create_time, IterDates, P
+
+# Read data
+f = cdms2.open(data_sample(ncfile))
+u = f('u')
+v = f('v')
+f.close()
+
+# Transect specs
+tlons, tlats = transect_specs(u.getGrid(), lon0, lat0, lon1, lat1)
+ttimes = create_time(IterDates((time0, time1), len(tlons), closed=True))
+
+# Compute transect
+tu = transect(u, tlons, tlats, time=ttimes)
+tv = transect(v, tlons, tlats, time=ttimes)
+
+# Plot along time
+s = stick2(tu, tv, figsize=(8,3), title='Space-time transect of speed', 
+    show=False, top=0.85, quiver_width=0.002)
+
+# Add a small map to show the transect positions
+add_map_lines(u, tlons, tlats, map_zoom=0.5)
+P.show()
+
+## Save
+#figfile = code_base_name(ext='png')
+#if os.path.exists(figfile): os.remove(figfile)
+#s.savefig(figfile, pdf=True)
