e47d14504c6251b5ac7f116cecf59fc216743f66
stefraynaud <stephane.raynaud@gmail.com>
2014-08-06 13:13:05 +0000

    test: missing tests

--- test/test_regrid.py
+++ test/test_regrid.py
@@ -4,13 +4,18 @@ from utils import *
 class TestSequenceFunctions(VCTestCase):
 
     for test_name in [
+        'test_regrid_fortran_extrap1d',
+        'test_regrid_fortran_interp1d',
+        'test_regrid_fortran_interp1dx',
+        'test_regrid_fortran_interp1dxx',
+        'test_regrid_fortran_curv2rect',
+        'test_regrid_regrid1d',
+        'test_regrid_curvedinterpolator',
         'test_regrid_extend',
         'test_regrid_kriging_cloud_split',
         'test_regrid_kriging_variogram',
         'test_regrid_kriging_krig',
         'test_regrid_kriging_parallel',
-        'test_regrid_regrid1d',
-        'test_regrid_fortran_extrap1d',
         ]:
         exec(method_template.format(test_name))
         
