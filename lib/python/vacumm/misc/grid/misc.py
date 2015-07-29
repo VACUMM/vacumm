e6b6a52b19c77e8aaeeb8f0d4a7533a691e8b4f0
matt29 <matthieu.caillaud@gmail.com>
2015-07-29 12:18:48 +0000

    add isoslice function from OCTANT library :  get any slice from a variable given an isovalue of another variable

--- lib/python/vacumm/misc/grid/misc.py
+++ lib/python/vacumm/misc/grid/misc.py
@@ -61,7 +61,7 @@ isseq = operator.isSequenceType
 isnum = operator.isNumberType
 isdic= ismap = operator.isMappingType
 
-__all__ = ['isgrid', 'get_resolution', 'get_distances', 'get_closest', 'get_closest_depth', 'get_geo_area',
+__all__ = ['isoslice','isgrid', 'get_resolution', 'get_distances', 'get_closest', 'get_closest_depth', 'get_geo_area',
     'bounds2d', 'bounds1d', 'get_axis', 'get_grid', 'get_grid_axes', "gridsel",
     'grid2d', 'num2axes2d', 'var2d', 'axis1d_from_bounds', 'get_xy',
     'deg2xy', 'set_grid', 'check_xy_shape', 'axes2d', 'meshgrid','meshbounds', 'meshweights',
@@ -72,6 +72,59 @@ __all__ = ['isgrid', 'get_resolution', 'get_distances', 'get_closest', 'get_clos
     'merge_axis_slice', 'merge_axis_slices', 'get_zdim', 'coord2slice', 'mask2ind',
     'varsel']
 __all__.sort()
+
+
+
+def isoslice(var,prop,isoval=0,axis=0,masking=True):
+    """
+    result = isoslice(variable,property[,isoval=0])
+    
+    result is a a projection of variable at property == isoval in the first
+    nonsingleton dimension.  In the case when there is more than one zero
+    crossing, the results are averaged.
+    
+    EXAMPLE:
+    Assume two three dimensional variable, s (salt), z (depth), and
+    u (velicity), all on the same 3D grid.  x and y are the horizontal 
+    positions, with the same horizontal dimensions as z (the 3D depth 
+    field).  Here, assume the vertical dimension, that will be projected,
+    is the first.  
+    
+    s_at_m5  = isoslice(s,z,-5);        # s at z == -5
+    h_at_s30 = isoslice(z,s,30);       # z at s == 30
+    u_at_s30 = isoslice(u,s,30);       # u at s == 30
+
+    Get from OCTANT library : https://github.com/hetland/octant
+    """
+    if (len(var.squeeze().shape)<=2):
+        raise ValueError, 'variable must have at least two dimensions'
+    if not prop.shape == var.shape:
+        raise ValueError, 'dimension of var and prop must be identical'
+    var = var.swapaxes(0, axis)
+    prop = prop.swapaxes(0, axis)
+    prop=prop-isoval
+    sz = N.shape(var)
+    var = var.reshape(sz[0],-1)
+    prop = prop.reshape(sz[0],-1)
+    #find zero-crossings (zc == 1)
+    zc =  N.where( (prop[:-1,:]*prop[1:,:])<0.0 ,1.0, 0.0)
+    varl = var[:-1,:]*zc
+    varh = var[1:,:]*zc
+    propl = prop[:-1,:]*zc
+    proph = prop[1:,:]*zc
+    result = varl - propl*(varh-varl)/(proph-propl)
+    result = N.where(zc==1., result, 0.)
+    szc = zc.sum(axis=0)
+    szc = N.where(szc==0., 1, szc)
+    result = result.sum(axis=0)/szc
+    if masking:
+        result = N.ma.masked_where(zc.sum(axis=0)==0, result)
+        if all(result.mask):
+            raise Warning, 'property==%f out of range (%f, %f)' % \
+                           (isoval, (prop+isoval).min(), (prop+isoval).max())
+    result = result.reshape(sz[1:])
+    return(result)
+
 
 def isgrid(gg, curv=None):
     """Check if gg is a grid
