a1b8230ce8a8d1e160014fa32218564f104769ca
stefraynaud <stephane.raynaud@gmail.com>
2016-03-10 16:40:53 +0000

    Added closing after showing in core_plot

--- lib/python/vacumm/misc/core_plot.py
+++ lib/python/vacumm/misc/core_plot.py
@@ -1255,7 +1255,7 @@ class Plot(object):
         if show:
             self.show(**kw['show'])
 
-        elif close:
+        if close:
             self.close()
 
 
@@ -3408,6 +3408,13 @@ class ScalarMappable:
             - **nocache**, optional: Once levels are computed, they are stored
               in cache. If ``nocache is True``, first check cache before
               trying to compute levels.
+            - **autoscaling**, optional: Autoscaling mode.
+
+                - ``"normal"``: Use :func:`~vacumm.misc.misc.auto_scale`.
+                - ``"degrees"``: Use :func:`~vacumm.misc.misc.geo_scale`.
+                - `A callable: Use it to auto scale. It should accept
+                  the follwing keywords: vmin, vmax, nmax, keepminmax.
+             
         """
         # Cache
         levels = self.get_obj('levels')
