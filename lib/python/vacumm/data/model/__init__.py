aceeeb1d28290c0bc36625ba7fd9e64a30e740f7
Stephane Raynaud <stephane.raynaud@gmail.com>
2017-05-03 14:31:47 +0200

    Fix locale set

--- lib/python/vacumm/data/model/__init__.py
+++ lib/python/vacumm/data/model/__init__.py
@@ -39,7 +39,10 @@ model tools
 # import mars
 import os as _os, locale as _locale
 _os.environ['LC_NUMERIC'] = 'en_US.UTF-8'
-_locale.setlocale(_locale.LC_NUMERIC, 'en_US.UTF-8')
+try:
+    _locale.setlocale(_locale.LC_NUMERIC, 'en_US.UTF-8')
+except:
+    pass
 
 import cfsr
 import globcurrent
