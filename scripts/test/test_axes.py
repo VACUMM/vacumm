b5abefa2a871e9daa841822f9d732a3f1eeef1e3
Stephane Raynaud <stephane.raynaud@gmail.com>
2018-10-04 16:07:44 +0200

    Add test_axes

--- scripts/test/test_axes.py
+++ scripts/test/test_axes.py
@@ -1,12 +1,40 @@
 """Test the :class:`~vacumm.misc.axes` module"""
 from vacumm.misc.axes import (islon, islat, isdep, isforecast, istime,
                               create_lon, create_lat, create_dep,
-                              create_time, create_forecast)
+                              create_alt, isalt,
+                              create_time, create_forecast, create_axis)
 
 
-#%% Create
+#%% Longitude
 lon = create_lon(range(5), long_name='My longitudes')
 assert lon.id == 'lon'
 assert lon.long_name == 'My longitudes'
 assert islon(lon)
+lon = create_axis(range(2))
+lon.units = 'degrees_east'
+assert islon(lon)
+
+#%% Latitude
+lat = create_lat(range(5))
+assert lat.axis == 'Y'
+assert islat(lat)
+
+#%% Depth
+dep = create_dep(range(5))
+assert dep.units == 'm'
+assert isdep(dep)
+
+#%% Altitude
+alt = create_alt(range(5))
+assert alt.units == 'm'
+assert isalt(alt)
+
+#%% Time
+time = create_time(['2000', '2001'], units='years since 1990')
+assert istime(time)
+assert time[0] == 10
+
+#%% Forecast
+forecast = create_forecast(range(5), units='days')
+assert isforecast(forecast)
 
--- scripts/test/test_cf_specs.py
+++ scripts/test/test_cf_specs.py
@@ -1,31 +1,29 @@
-"""Test the :class:`~vacumm.data.cf.VarSpecs` and :class:`~vacumm.data.cf.AxisSpecs` classes"""
-from vcmq import code_file_name
-from vacumm.data.cf import CF_VAR_SPECS
-# TODO: test_cf_specs to complete
+"""Test the :class:`~vacumm.misc.cf.VarSpecs` and :class:`~vacumm.misc.cf.AxisSpecs` classes"""
+from vcmq import code_file_name, MV2
+from vacumm.misc.cf import CF_VAR_SPECS
 
-# Content
+#%% Content
 assert 'temp' in CF_VAR_SPECS
 ts = CF_VAR_SPECS['temp']
-assert isinstance(ts ['id'], list)
+assert isinstance(ts['id'], list)
 assert ts['id'][0] == 'temp'
 assert ts['long_name'][0] == 'Temperature'
 
-
-# Register new variable from direct specs
+#%% Register new variable from direct specs
 CF_VAR_SPECS.register('banana', long_name='Good bananas')
 assert 'banana' in CF_VAR_SPECS
 assert 'Good bananas' in CF_VAR_SPECS['banana']['long_name']
 
-# Change existing variable
+#%% Change existing variable
 CF_VAR_SPECS.register('temp', long_name='Another temperature')
-assert 'TEMP' in CF_VAR_SPECS['temp']['id'] # keep default
+assert 'TEMP' in CF_VAR_SPECS['temp']['id']  # keep default
 assert 'Another temperature' in CF_VAR_SPECS['temp']['long_name']
-assert 'banana' in CF_VAR_SPECS # don't erase
+assert 'banana' in CF_VAR_SPECS  # don't erase
 
-# Register from a config file
+#%% Register from a config file
 cfgfile = code_file_name(ext='cfg')
 CF_VAR_SPECS.register_from_cfg(cfgfile)
 assert "funnyvar" in CF_VAR_SPECS
 assert "nicetemp" in CF_VAR_SPECS
+assert CF_VAR_SPECS["nicetemp"]['standard_name'][0] == "nice_temperature"
 assert "temp" in CF_VAR_SPECS["nicetemp"]['id']
-
